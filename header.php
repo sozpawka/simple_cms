e55ce586cfc2822932aa07468658cc05a97174dc
Sozpawka <sozpawkawar@gmail.com>
2026-06-06 15:56:01 +0700

    1_commit_database_connect

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>3 laba</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <h1>CMS</h1>
+</header>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+require_once 'database.php';
+require_once 'header.php';
+?>
+
+<h2>Главная страница</h2>
+
+<?php require_once 'footer.php'; ?>
